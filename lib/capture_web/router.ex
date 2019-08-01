2fa52bddda26065fc03c20da0286b1da7e4080aa
Jody Vandenschrick <jody.vandenschrick@cultureamp.com>
2019-08-01 11:12:44 +1000

    Add Jason package to decode http json requests

--- lib/capture_web/router.ex
+++ lib/capture_web/router.ex
@@ -1,6 +1,10 @@
 defmodule CaptureWeb.Router do
   use Plug.Router
 
+  plub Plug.Logger, log: :debug
+  plug Plug.Parsers, parsers: [:urlencoded, :json]
+  json_decoder: Jason
+
   alias Capture.{Response, Responses, Repo}
 
   plug :match
